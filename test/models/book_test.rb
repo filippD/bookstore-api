75c9fdd6fcd2246e8d562337b6e7cb3f3b020e77
YellowPipe <philippdmitrienko@gmail.com>
2019-08-01 14:02:35 +0300

    added tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(username: 'John', email: 'john@example.com')
+  	@book = Book.new(title: 'Book', category: 'Action', read: false, user: @user)
+  end
+
+  test 'valid book' do
+    assert @book.valid?
+  end
+
+  test 'invalid without title' do
+  	@book.title = nil 
+    refute @book.valid?, 'book is valid without a title'
+    assert_not_nil @book.errors[:title], 'no validation error for title present'
+  end
+
+  test 'invalid without category' do
+  	@book.category = nil
+    refute @book.valid?, 'book is valid without a category'
+    assert_not_nil @book.errors[:category], 'no validation error for category present'
+  end
+
+  test 'belongs to a user' do
+  	assert @book.user == @user
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(username: 'John', email: 'john@example.com', password: 'secret', password_confirmation: 'secret')
+  end
+
+  test 'valid user' do
+  	@user.save!
+    assert @user.valid?
+  end
+
+  test 'invalid without username' do
+  	@user.username = nil 
+    refute @user.valid?, 'user is valid without a name'
+    assert_not_nil @user.errors[:name], 'no validation error for name present'
+  end
+
+  test 'invalid without email' do
+  	@user.email = nil
+    refute @user.valid?, 'user is valid without a category'
+    assert_not_nil @user.errors[:email], 'no validation error for email present'
+  end
+
+  test 'invalid without password' do
+  	@user.password = nil
+    refute @user.valid?, 'user is valid without a category'
+    assert_not_nil @user.errors[:password], 'no validation error for password present'
+  end
+
+  test 'invalid when passwords do not match' do
+  	@user.password_confirmation = "different"
+    refute @user.valid?, 'user is valid without a matching passwords'
+  end
+
+  test 'can create posts' do
+  	@user.save
+  	assert_difference('@user.books.count', 1) do
+	    @user.books.create(title: 'Book', category: 'Action', read: false)
+	end
+  end
+
 end
