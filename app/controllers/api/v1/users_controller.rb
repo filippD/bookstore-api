6bab2a2bfa16b903a8b1aabb6aaca72f1a58451b
YellowPipe <philippdmitrienko@gmail.com>
2019-08-01 13:15:49 +0300

    registration now returns token for immediate login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,8 @@ module Api::V1
 	    def create
 	    	@user = User.new(user_params)
 			if @user.save
-				render json: @user, status: :created
+				auth_token = Knock::AuthToken.new payload: { sub: @user.id }
+      			render json: auth_token, status: :created
 			else
 				render json: @user.errors, status: :unprocessable_entity
 			end
